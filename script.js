2555a4b232a9bd80d38f704c95134f81d3d69fda
Yomawa <bogeski.maja@gmail.com>
2015-08-17 16:15:14 -0700

    flashingColors

--- script.js
+++ script.js
@@ -1,67 +1,47 @@
 // Your JS goes here
-/*var newText =document.createElement("h1");//create element
-newText.inerText="Hello there";//call element that has difined set some thext
-newText.style.color="red";
 
-var parent = document.getElementsByTagName("h1")[0];//get element by tag name
-parent.appendChild(newText);//appendChiled it is called
+window.onload= function(){
+	setInterval(function(){
+		reStart();
+	},2000);
 
-
-/*if we do not set verilbe together
-
-document.querySelector("h1").appendChiled(newElement);
+	start();
+	
 
 
-
-*/
-/*chaning elements together YOU CAN NOT TO THIS TOGETHER*/
+};
 
 
-/*document.createElement = document.createElement("h1").innerText = "Hello";*/
 
-/*var newDiv = document.createElement("div");
-newDiv.style.width="50px";
-newDiv.style.height="50px";
-newDiv.style.float="left";
-newDiv.style.backgroundColor = "red";
-document.body.appendChild(newDiv);
+/*lashing Colors*/
 
-var div2 = document.createElement("div");
-div2.style.width="50px";
-div2.style.height="50px";
-div2.style.float="left";
-div2.style.backgroundColor = "blue";
-document.body.appendChild(div2);*/
-
-/*var printDiv = 0;
-function print(){
-	if(printDiv % 2 === 0){
-		console.log("newDiv");
-		printDiv++;
-	}
-	else{
-		console.log("div2");
-	}
-	for (newDiv = 0; newDiv<10; newDiv++){
-		console.log("newDiv");
+function start(){
+	for (var i=0; i<81; i++){
+		var div = document.createElement("div");
+		div.style.width="11.1%";
+		div.style.paddingBottom="11.1%";
+		div.style.float="left";
+		document.body.appendChild(div);
 	}
 }
-*/
-
-for (var i=0; i<63; i++){
-	var div = document.createElement("div");
-	div.style.width="11.1%";
-	div.style.paddingBottom="11.1%";
-	div.style.float="left";
-	/*div.style.backgroundColor="red";*/
-	document.body.appendChild(div);
+function reStart(){
+	for (var i=0; i<81; i++){
+		if(i%2===0){
+			document.getElementsByTagName("div")[i].style.backgroundColor="#" + (Math.round(Math.random() * 0XFFFFFF)).toString(16);
+		}
+		else{
+			document.getElementsByTagName("div")[i].style.backgroundColor="#" + (Math.round(Math.random() * 0XFFFFFF)).toString(16);
+		}
+	}
 	
-	if(i%2===0){
-		div.style.backgroundColor="black";
-	}
-	else{
-		div.style.backgroundColor="red";
-	}
-	}
 
-	
+
+}
+
+
+
+
+
+
+
+
